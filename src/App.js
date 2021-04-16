96d4598ea8f3417a894cc16868114c925bf5f443
xxalsgh0203 <music4537@naver.com>
2021-04-16 15:13:07 +0900

    made shoes list into components

--- src/App.js
+++ src/App.js
@@ -1,7 +1,11 @@
 import './App.css';
+import React, {useState} from 'react';
 import { Navbar, Nav, NavDropdown, Jumbotron, Button} from 'react-bootstrap';
-
+import ProductInfo from './data';
 function App() {
+
+  let [shoes, changeShoes] = useState(ProductInfo);
+
   return (
     <div className="App">
       <Navbar bg="light" expand="lg" className="nav-bar">
@@ -34,25 +38,30 @@ function App() {
 
       <div className="container"> {/*좌우 여백을 예쁘게 잡아줌 (Bootstrap 문법)*/}
         <div className="row"> {/*사이트를 열두개의 column 으로 쪼개겠습니다 */}
-          <div className="col-md-4"> {/*12개 중 4개의 column 을 사용 (3등분) + 모바일에서 세로정렬 */}
-            <img src="https://codingapple1.github.io/shop/shoes1.jpg" width="100%"/>
-            <h4>상품명</h4>
-            <p>상품설명 & 가격</p>
-          </div>
-          <div className="col-md-4">
-            <img src="https://codingapple1.github.io/shop/shoes2.jpg" width="100%"/>
-            <h4>상품명</h4>
-            <p>상품설명 & 가격</p>
-          </div>
-          <div className="col-md-4">
-            <img src="https://codingapple1.github.io/shop/shoes3.jpg" width="100%"/>
-            <h4>상품명</h4>
-            <p>상품설명 & 가격</p>
-          </div> 
+        
+          {
+            shoes.map(function(shoe, i){
+              return (
+                <ShoesinStock shoes={ shoe } i={i} key={i}/>
+              )  
+            })
+          }
+
         </div>
       </div>
     </div>
   );
+}
+
+function ShoesinStock(props) {
+  return (
+    <div className="col-md-4"> {/*12개 중 4개의 column 을 사용 (3등분) + 모바일에서 세로정렬 */}
+      <img src={ 'https://codingapple1.github.io/shop/shoes' + (props.i + 1) + '.jpg' } width="100%" />
+      <h4>{ props.shoes.title }</h4>
+      <p>{ props.shoes.content }</p>
+      <p>{ props.shoes.price }</p>
+    </div>
+  )
 }
 
 export default App;
---
+++ src/data.js
@@ -0,0 +1,22 @@
+export default [
+    {
+      id : 0,
+      title : "White and Black",
+      content : "Born in France",
+      price : 10 + '$'
+    },
+  
+    {
+      id : 1,
+      title : "Red Knit",
+      content : "Born in Seoul",
+      price : 20 + '$'
+    },
+  
+    {
+      id : 2,
+      title : "Grey Yordan",
+      content : "Born in the States",
+      price : 30 + '$'
+    }
+  ] 
