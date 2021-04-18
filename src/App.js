11da3de6e4d321100e56f86506168bd713a4c693
xxalsgh0203 <music4537@naver.com>
2021-04-18 11:01:10 +0900

    Routed paths and usedLink, Switch, history function

--- src/App.js
+++ src/App.js
@@ -2,6 +2,9 @@ import './App.css';
 import React, {useState} from 'react';
 import { Navbar, Nav, NavDropdown, Jumbotron, Button} from 'react-bootstrap';
 import ProductInfo from './data';
+import { Link, Route, Switch } from 'react-router-dom';
+import Detail from './Detail.js'
+
 function App() {
 
   let [shoes, changeShoes] = useState(ProductInfo);
@@ -13,8 +16,8 @@ function App() {
         <Navbar.Toggle aria-controls="basic-navbar-nav" />
         <Navbar.Collapse id="basic-navbar-nav">
           <Nav className="ml-auto">
-            <Nav.Link href="#home">Home</Nav.Link>
-            <Nav.Link href="#link">Link</Nav.Link>
+            <Nav.Link><Link to="/">Home</Link></Nav.Link>
+            <Nav.Link><Link to="/detail">Detail</Link></Nav.Link>
             <NavDropdown title="Dropdown" id="basic-nav-dropdown">
               <NavDropdown.Item href="#action/3.1">Action</NavDropdown.Item>
               <NavDropdown.Item href="#action/3.2">Another action</NavDropdown.Item>
@@ -26,29 +29,36 @@ function App() {
         </Navbar.Collapse>
       </Navbar>
 
-      <Jumbotron className="jumbo">
-        <h1>20% Season OFF</h1>
-        <p>
-          with a $120 purchase. Choose your color & size, add to cart, & use code SHOECOMP. While supplies last
-        </p>
-        <p>
-          <Button variant="primary">Learn more</Button>
-        </p>
-      </Jumbotron>
 
-      <div className="container"> {/*좌우 여백을 예쁘게 잡아줌 (Bootstrap 문법)*/}
-        <div className="row"> {/*사이트를 열두개의 column 으로 쪼개겠습니다 */}
-        
-          {
-            shoes.map(function(shoe, i){
-              return (
-                <ShoesinStock shoes={ shoe } i={i} key={i}/>
-              )  
-            })
-          }
+      <Switch> {/*하나의 Route만 보여주도록 함 */}
+        <Route exact path="/">
+          <Jumbotron className="jumbo">
+            <h1>20% Season OFF</h1>
+            <p>
+              with a $120 purchase. Choose your color & size, add to cart, & use code SHOECOMP. While supplies last
+            </p>
+            <p>
+              <Button variant="primary">Learn more</Button>
+            </p>
+          </Jumbotron>
 
-        </div>
-      </div>
+          <div className="container"> {/*좌우 여백을 예쁘게 잡아줌 (Bootstrap 문법)*/}
+            <div className="row"> {/*사이트를 열두개의 column 으로 쪼개겠습니다 */}
+            {
+              shoes.map(function(shoe, i){
+                return (
+                  <ShoesinStock shoes={ shoe } i={i} key={i}/>
+                )  
+              })
+            }
+            </div>
+          </div>
+        </Route>
+
+        <Route path="/detail">
+            <Detail />
+        </Route>
+      </Switch>        
     </div>
   );
 }
