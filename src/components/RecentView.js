961fd2559ec74859e580f73b6a42a0dd254f30fe
xxalsgh0203 <music4537@naver.com>
2021-04-29 22:55:57 +0900

    revised some errors on reviewed item page

--- src/components/RecentView.js
+++ src/components/RecentView.js
@@ -1,24 +1,32 @@
 import React from 'react';
+import ShoesinStock from './ShoesinStock.js'
 
 function RecentView(props){
     let vieweditem = localStorage.getItem('viewed');
     vieweditem = JSON.parse(vieweditem);
-
-    let productDetailFound = props.shoes.find(function(product){
-        return product.id==vieweditem[0];
-    });
     return (
         <div>
-            <p>{productDetailFound.title}</p>
-            <p>{productDetailFound.content}</p>
-            <p>{productDetailFound.price}</p>
+            <div className="my-alert-yellow">
+                <p>Recent viewed items</p>
+            </div>
+
+            <div className="container"> {/*좌우 여백을 예쁘게 잡아줌 (Bootstrap 문법)*/}
+                <div className="row"> {/*사이트를 열두개의 column 으로 쪼개겠습니다 */}
+                {
+                    vieweditem.map(function(shoe, i){
+                        let productDetailFound = props.shoes.find(function(product){
+                            return product.id==shoe
+                        });
+
+                        return (
+                            <ShoesinStock shoes={ productDetailFound } i={i} key={i}/>
+                        )  
+                    })
+                }
+                </div>
+            </div>
         </div>
     )
-
-
-    // vieweditem.map(function(item, i){
-
-    // })
 }
 
 export default RecentView;
--- src/components/ShoesinStock.js
+++ src/components/ShoesinStock.js
@@ -1,10 +1,11 @@
 import React, {useState} from 'react';
 import { useHistory } from 'react-router-dom';
+
 function ShoesinStock(props) {
   let history = useHistory();
     return (
       <div className="col-md-4" onClick={()=>{ history.push('/detail/' + props.shoes.id)}}>{/*12개 중 4개의 column 을 사용 (3등분) + 모바일에서 세로정렬 */}
-        <img src={ 'https://codingapple1.github.io/shop/shoes' + (props.i + 1) + '.jpg' } width="100%"/>
+        <img src={ 'https://codingapple1.github.io/shop/shoes' + (props.shoes.id + 1) + '.jpg' } width="100%"/>
         <h4>{ props.shoes.title }</h4>
         <p>{ props.shoes.content }</p>
         <p>{ props.shoes.price }</p>
